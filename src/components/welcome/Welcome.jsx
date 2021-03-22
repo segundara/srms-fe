f8ecf477af38d95dbcd2ec2c6f6dddc1f594b178
segundara <segundara@gmail.com>
2021-03-22 12:37:35 +0200

    update to welcome page

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -4,9 +4,9 @@ import "../commonStyle/style.scss";
 
 const WelcomePage = () => {
     return (
-        <Jumbotron className="shadow-lg jumbo text-center">
+        <Jumbotron className="jumbo d-flex">
             <div className="d-flex flex-column">
-                <span className="mt-5">Welcome to the School Portal.</span>
+                <span>Welcome to the School Portal.</span>
                 <span>Login details can be found in <a href="https://srms-be.herokuapp.com/api-docs">api-docs</a></span>
             </div>
         </Jumbotron>
